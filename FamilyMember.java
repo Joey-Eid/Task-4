4e7ae644b4722b12ca55931666fa53a829be4498
Youssef Eid <90372823+Joey-Eid@users.noreply.github.com>
2021-10-06 00:55:06 +0200

    Add files via upload

---
+++ FamilyMember.java
@@ -0,0 +1,28 @@
+package HackerRank;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface FamilyBudget {
+	String userRole() default "GUEST";
+    int budgetLimit() default 0;
+}
+
+class FamilyMember {
+    @FamilyBudget(userRole = "SENIOR", budgetLimit = 100)
+	public void seniorMember(int budget, int moneySpend) {
+		System.out.println("Senior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+
+    @FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
+	public void juniorUser(int budget, int moneySpend) {
+		System.out.println("Junior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+}
